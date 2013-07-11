9feca821c4f171ef9c7a01543d754480c2d0db5d
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-11 19:36:07 -0400

    Initializing repo and repairing database.yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BooksTdd::Application.config.secret_token = '8ffb078b10f3e508b60ba832a906914660735166b4f4842e5d8083ff5222a5eccb61ed5c9f890afbd67d42b78eb37b861a25a2d9f23feff9b3eea0b166751ae8'
